8651aa71eb2e04eb216f8068c94885dda55dcc31
krystal3l <8uartz@gmail.com>
2020-01-17 12:18:09 +0900

    added Custom User Admin

--- users/admin.py
+++ users/admin.py
@@ -1,7 +1,44 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
-# Register your models here.
+
+@admin.register(models.User)
+class CustomUserAdmin(UserAdmin):
+
+    """ Custom User Admin """
+
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthday",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
+
+
+'''
+# decorator : 반드시 class 위에 작성
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin """
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
+
+# admin.site.register(models.User, CustomUserAdmin)
+'''
