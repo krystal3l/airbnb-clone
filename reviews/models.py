01db940521256f0bda29f4109ca567d8c04b0612
krystal3l <8uartz@gmail.com>
2020-01-20 13:38:00 +0900

    added review rating average

--- reviews/models.py
+++ reviews/models.py
@@ -14,9 +14,26 @@ class Review(core_models.TimeStampModel):
     check_in = models.IntegerField()
     value = models.IntegerField()
     # user가 삭제되면 리뷰도 같이 삭제돼야함. 따라서 CASCADE
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
-    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        "users.User", related_name="reviews", on_delete=models.CASCADE
+    )
+    room = models.ForeignKey(
+        "rooms.Room", related_name="reviews", on_delete=models.CASCADE
+    )
 
     def __str__(self):
         # fk로 설정한 값을 뿌려줄 수 있음.
         return f"{self.review} - {self.room}"
+
+    def rating_average(self):
+        avg = (
+            self.accuracy
+            + self.communication
+            + self.cleanliness
+            + self.location
+            + self.check_in
+            + self.value
+        ) / 6
+        return round(avg, 2)
+
+    rating_average.short_description = "Avg."
