812e76ee0b5a67e454ec413a454507a5e86b387f
krystal3l <8uartz@gmail.com>
2020-01-20 17:51:29 +0900

    added setting(relative path)

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    # 상대경로로 사용하기 위해. http://127.0.0.1:8000/media/avatars/avatar.jpg
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
