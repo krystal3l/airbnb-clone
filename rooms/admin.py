bb17166d3e5127172d25e2199100797e388bf166
krystal3l <8uartz@gmail.com>
2020-01-20 13:37:40 +0900

    added room admin panel

--- rooms/admin.py
+++ rooms/admin.py
@@ -7,6 +7,14 @@ class ItemAdmin(admin.ModelAdmin):
 
     """ Item Admin Definition """
 
+    list_display = (
+        "name",
+        "used_by",
+    )
+
+    def used_by(self, obj):
+        return obj.rooms.count()
+
     pass
 
 
@@ -15,7 +23,76 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")},),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths",)}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rules"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)},),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+        "count_photos",
+        "total_rating",
+    )
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city",
+        "country",
+    )
+
+    search_fields = (
+        "=city",
+        "^host__username",
+    )
+
+    # manytomanyfields만 가능
+    filter_horizontal = (
+        "amenities",
+        "facilities",
+        "house_rules",
+    )
+
+    # 정렬
+    ordering = ("name", "price", "bedrooms")
+
+    # amenities의 개수를 확인하기 위해 함수를 만듦.
+    # self : RoomAdmin class, obj : 객실 정보들!(admin 패널에 보이는 컬럼값들)
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+
+    def count_photos(self, obj):
+        return obj.photos.count()
+
+    # count_amenities 컬럼명 변경
+    # count_amenities.short_description = "hello"
 
 
 @admin.register(models.Photo)
