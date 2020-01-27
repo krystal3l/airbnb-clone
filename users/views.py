516a45d21fff238a050e84e0ffcd1c06deeaff69
krystal3l <8uartz@gmail.com>
2020-01-27 14:56:30 +0900

    Signup form

--- users/views.py
+++ users/views.py
@@ -48,3 +48,23 @@ def log_out(request):
     return redirect(reverse("core:home"))
 """
 
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    initial = {
+        "first_name": "krystal1",
+        "last_name": "kang1",
+        "email": "8uartz111@gmail.com",
+    }
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+        return super().form_invalid(form)
