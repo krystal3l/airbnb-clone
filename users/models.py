dff3626f70b7874208a3d1d0c9f55eb6b28fb9d4
krystal3l <8uartz@gmail.com>
2020-01-27 16:08:59 +0900

    Added default data

--- users/models.py
+++ users/models.py
@@ -33,8 +33,12 @@ class User(AbstractUser):
 
     birthday = models.DateField(blank=True, null=True)
 
-    language = models.CharField(choices=LANGUAGE_CHOICES, max_length=2, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=2, blank=True, default=LANGUAGE_KOREAN
+    )
 
-    currency = models.CharField(choices=CURRENCY_CHOICES, max_length=3, blank=True)
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=3, blank=True, default=CURRENCY_KRW
+    )
 
     superhost = models.BooleanField(default=False)
